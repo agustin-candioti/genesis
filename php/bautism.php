594cf58cb21386070e2d18057bc4401589e6dc81
agustin-candioti <agustin.candioti@hotmail.com>
2021-04-12 09:08:44 -0300

    fixed course images description. added footer and new html files

---
+++ php/bautism.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Neue:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@1,700&display=swap" rel="stylesheet">
+    <title>Génesis - Reservá tu Vuelo Bautismo</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="row">
+              <div class="col-sm-4">
+                <a href="./index.html"><img src="../img/genesis logo.jpg" alt="logo home" id="logoHome"></a>
+              </div>
+              <div class="col-sm-4"></div>
+              <div class="col-sm-4">
+                <ul>
+                    <li class="nav-item">
+                      <a href="#courses">Nuestros Cursos</a>
+                    </li>
+                    <li class="nav-item">
+                      <a href="#">Contacto</a>
+                    </li>
+                    <li class="nav-item">
+                      <a href="#">Preguntas Frecuentes</a>
+                    </li>
+                  </ul>
+                </div>
+            </div>
+        </nav>
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-12">
+              
+              </div>
+           </div>
+          </div>
+        </div>
+    </header>
+    
+</body>
+</html>
